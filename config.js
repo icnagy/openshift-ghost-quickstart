1d5c4b22f788b5eac6ddeaafe3027466d5ba1d4f
Csaba Nagy <Csaba.Nagy02@t-systems.com>
2013-10-19 12:58:34 +0200

    removing dns

--- config.js
+++ config.js
@@ -44,8 +44,8 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-#        url: 'http://'+process.env.OPENSHIFT_APP_DNS,
-        url: 'http://www.icnagy.com',
+        url: 'http://'+process.env.OPENSHIFT_APP_DNS,
+#        url: 'http://www.icnagy.com',
         mail: {
             transport: 'SMTP',
             options: {
@@ -55,7 +55,7 @@ config = {
                     pass: '1fgfhzt8hc94'
                 }
             }
-	},
+        },
         database: {
             client: 'sqlite3',
             connection: {
